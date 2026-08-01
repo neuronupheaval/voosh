2368584994dd6528df33c6b0df35d7187681a6be
Marcelo Shiniti Uchimura <marcelo@uchimura.io>
2026-08-01 09:06:24 -0300

    fix(no-css-issue): removed block from vite.config.js

--- voosh-file-xfer/vite.config.js
+++ voosh-file-xfer/vite.config.js
@@ -3,14 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [
     {
-      name: 'configure-cross-origin-isolation-headers',
-      configureServer(server) {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-          next();
-        });
-      },
       config(config, { command }) {
         if (command === "build" || command === "preview") {
           return {
